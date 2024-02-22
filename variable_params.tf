7a32e1e815f2653011c711a6a9cf059d51db5e65
Sujendra Gharat <sgharat297@gmail.com>
2024-02-21 20:51:07 -0500

    testing

--- variable_params.tf
+++ variable_params.tf
@@ -1,74 +1,142 @@
 variable "project_id" {
-  description = "The unique identifier for the Google Cloud Platform (GCP) project."
+  description = "id for the project"
   type        = string
 }
 
 variable "region" {
-  description = "The geographic region where resources will be provisioned within GCP."
+  description = "The GCP region to deploy resources"
   type        = string
 }
 
 variable "zone" {
-  description = "The specific zone within the chosen region where resources will be deployed."
+  description = "The GCP zone to deploy resources"
   type        = string
 }
 
-variable "vpc_name" {
-  description = "The name of the Virtual Private Cloud (VPC) network."
+variable "credentials_file" {
+  description = "File have gcp credentials for seervice account"
   type        = string
 }
 
-variable "web_subnet_name" {
-  description = "The name of the subnet for the web application servers."
-  type        = string
+variable "vpcs" {
+  description = "A list of objects representing VPC configurations"
+  type = list(object({
+    name = string
+    # description = "The name of the VPC"
+    vpc_name = string
+    # description = "The name of web subnet"
+    websubnet_name = string
+    # description = "The name of db subnet"
+    dbsubnet_name = string
+    # description = "CIDR for the webapp subnet"
+    webapp_subnet_cidr = string
+    # description = "CIDR for the db subnet"
+    db_subnet_cidr = string
+    # description = "CIDR for the webapp subnet route"
+    websubnetroutename = string
+    # description = "To set private ip google access of subnets to on or off"
+    privateipgoogleaccess = bool
+  }))
 }
-
-variable "db_subnet_name" {
-  description = "The name of the subnet for the database servers."
-  type        = string
-}
-
-variable "web_app_route_name" {
-  description = "The name of the route for directing traffic to the web application subnet."
-  type        = string
-}
-
-variable "web_app_cidr" {
-  description = "The Classless Inter-Domain Routing (CIDR) block for the web application subnet."
-  type        = string
-}
-
-variable "db_cidr" {
-  description = "The CIDR block for the database subnet."
-  type        = string
-}
-
 variable "web_app_route_cidr" {
-  description = "The CIDR block for the route directing traffic to the web application subnet."
+  description = "CIDR for the webapp subnet route"
   type        = string
 }
 
 variable "auto_create_subnets" {
-  description = "Set to true to automatically create subnets, false otherwise."
+  description = "To set value to true or false for automatically creating subnets"
   type        = bool
 }
 
 variable "delete_default_routes" {
-  description = "Set to true to delete default routes, false otherwise."
+  description = "To set value to true or false to delete default routes"
   type        = bool
 }
 
 variable "routing_mode" {
-  description = "The routing mode for the VPC, e.g., 'GLOBAL' or 'REGIONAL'."
+  description = "To set routing mode"
   type        = string
 }
 
-variable "private_ip_google_access" {
-  description = "Enable or disable access to Google services using private IP addresses."
-  type        = bool
+variable "next_hop_gateway" {
+  description = "To set next hop gateway value"
+  type        = string
 }
 
-variable "next_hop_gateway" {
-  description = "The next hop gateway used for routing traffic."
+variable "credentials_file" {
+  description = "Path to the Google Cloud Platform service account key file"
+  type        = string
+}
+
+
+ 
+variable "webapp_firewall_name" {
+  description = "Name of the firewall for the web application"
+  type        = string
+}
+ 
+variable "webapp_firewall_protocol" {
+  description = "Protocol for the web application firewall rule (e.g., tcp, udp)"
+  type        = string
+}
+ 
+variable "webapp_firewall_protocol_allow_ports" {
+  description = "List of ports to allow for the web application firewall rule"
+  type        = list(string)
+}
+ 
+variable "webapp_firewall_source_tags" {
+  description = "Source tags for the web application firewall rule"
+  type        = list(string)
+}
+ 
+variable "webapp_firewall_target_tags" {
+  description = "Target tags for the web application firewall rule"
+  type        = list(string)
+}
+ 
+variable "webapp_subnet_ssh" {
+  description = "Name of the firewall rule for SSH access to the web application subnet"
+  type        = string
+}
+ 
+variable "webapp_firewall__protocol_deny_ports" {
+  description = "List of ports to deny for the web application SSH firewall rule"
+  type        = list(string)
+}
+ 
+variable "compute_instance_name" {
+  description = "Name of the compute instance for the web application"
+  type        = string
+}
+ 
+variable "machine_type" {
+  description = "Machine type for the compute instance"
+  type        = string
+}
+ 
+ 
+variable "instance_image" {
+  description = "URL of the image for the compute instance"
+  type        = string
+}
+ 
+variable "instance_size" {
+  description = "Size of the boot disk for the compute instance (in GB)"
+  type        = number
+}
+ 
+variable "webapp_bootdisk_type" {
+  description = "Type of the boot disk for the compute instance"
+  type        = string
+}
+ 
+variable "compute_instance_tags" {
+  description = "Tags for the compute instance"
+  type        = list(string)
+}
+ 
+variable "network_tier" {
+  description = "Network tier for the compute instance"
   type        = string
 }
